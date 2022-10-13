54a77874125fde4ad0fec6a97bcbe6d8b8ca7ad2
lordpazifist <111981114+lordpazifist@users.noreply.github.com>
2022-10-13 21:33:43 +0200

    Add files via upload

--- tumbleweed_afterinstall.sh
+++ tumbleweed_afterinstall.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+#Irgendwie war noch USB-Stick repo drin. die musste raus.
+sudo zypper removerepo openSUSE-20220930-0
+
 #set hostname
-sudo hostnamectl set-hostname VMTumbleweed
+sudo hostnamectl set-hostname XMGp507Tumbleweed
+
+#remove bloat
+sudo zypper remove -u discover
+
+#lock packes and patterns so that programs are only updated, not installed
+sudo zypper addlock discover
+sudo zypper addlock -t pattern games
+sudo zypper addlock -t pattern kde_pim
+
+#Switch to current Snapshot
+sudo zypper dup
+
+sudo zypper install rkhunter
+sudo rkhunter --update
+sudo rkhunter --propupd
+#c for check q for skip keypress
+sudo rkhunter -c -sk
 
 #third party repo
 sudo zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials' packman_essentials
@@ -13,30 +33,65 @@ sudo zypper addrepo https://download.opensuse.org/repositories/shells:zsh-users:
 
 sudo zypper refresh
 
-sudo zypper dist-upgrade --from packman_essentials --allow-vendor-change
+#change all essentials to packman. includes mesa since vaapi is disabled --> do not perform
+#sudo zypper dist-upgrade --from packman_essentials --allow-vendor-change
 
-sudo zypper install --from packman_essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full vlc-codecs#sudo zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials' packman_essentials
+#change only specified packages to packman
+sudo zypper install --from packman_essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full vlc-codecs vlc
 
-
-sudo zypper install vlc rkhunter thunderbird zsh zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search calibre clipgrab clamav xlsclients keepassxc discord virt-manager make gcc dkms kernel-devel chromium
+sudo zypper install git thunderbird zsh zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search clipgrab clamav xlsclients keepassxc discord virt-manager patterns-server-kvm_tools patterns-server-kvm_server chromium flatpak calibre dkms
 
 #enable wayland in different programs
 mkdir -p ~/.config/environment.d/
 echo "MOZ_ENABLE_WAYLAND=1" >> ~/.config/environment.d/envvars.conf
 
 sudo firewall-cmd --set-default-zone block
+#virensignaturen aktualisieren
+sudo freshclam
 
 #opensuse doesn'T have obs-studio -- flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.obsproject.Studio
 
-#nvidia
-#zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
-
-
 #zsh will be activated after restart or relogon
-cp .zshrc ~/
+cp zshrc ~/.zshrc
 #to be able to chsh
 echo enter user password
 chsh -s /bin/zsh
 sudo chsh -s /bin/zsh
+
+#virtual machines
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+sudo systemctl enable dkms.service
+sudo systemctl start dkms.service
+
+#https://github.com/tuxedocomputers/tuxedo-keyboard
+sudo zypper install make gcc kernel-devel
+git clone https://github.com/tuxedocomputers/tuxedo-keyboard.git ~/git_clones/tuxedo-keyboard
+cd ~/git_clones/tuxedo-keyboard
+git checkout release
+make clean
+sudo make dkmsinstall
+sudo modprobe tuxedo_keyboard
+
+#copy thunderbird-profiles
+cp -r ~/install/.thunderbird ~/
+
+#nvidia
+#zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
+#or directly from nvidia:
+sudo zypper install kernel-devel kernel-source gcc make dkms acpid libglvnd libglvnd-devel
+echo 'blacklist nouveau' | sudo tee -a /etc/modprobe.d/nvidia.conf
+echo 'add_drivers+=" nvidia nvidia_modeset nvidia_uvm nvidia_drm "' | sudo tee -a /etc/dracut.conf.d/nvidia.conf
+#Download nvidia driver from https://www.nvidia.de/Download/index.aspx?lang=de
+#safe "run" file in install directory
+chmod +x ~/install/NVIDIA-Linux-x86_64-515.76.run
+#boot into cmd without nouveau by adding nomodeset 3 to grub entry during boot
+#login with root
+#then run NVIDIA Installer. dont blacklist (was already done above), no xconf, yes to dkms
+sudo sh NVIDIA-Linux-x86_64-515.76.run
+sudo zypper install suse-prime
+#sudo dracut -f
+#reboot
