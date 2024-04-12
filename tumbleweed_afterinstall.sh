ec3328646cba4b4f1397f1225c175c8028fd09f3
nomispaz <111981114+nomispaz@users.noreply.github.com>
2024-04-12 20:48:25 +0200

    Update tumbleweed_afterinstall.sh

--- tumbleweed_afterinstall.sh
+++ tumbleweed_afterinstall.sh
@@ -35,55 +35,47 @@ sudo zypper addrepo https://rpm.tuxedocomputers.com/opensuse/15.5/repo-tuxedo-co
 
 sudo zypper refresh
 
-#change only specified packages to packman
-sudo zypper install --from packman_essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full vlc-codecs vlc
-
+# install various programs
 sudo zypper install git thunderbird clipgrab clamav keepassxc virt-manager patterns-server-kvm_tools patterns-server-kvm_server chromium flatpak calibre dkms screenfetch osc spec-cleaner testdisk screenfetch veracrypt neovim emacs fish alacritty discord gcc gcc-c++
 
+#tuxedo-keyboard an tuxedo-control-center via tuxedo-repos
+sudo zypper install tuxedo-control-center tuxedo-drivers
+
+#nvidia now via repos (open-kernel-modules)
+sudo zypper install nvidia-open-driver-G06-signed-kmp-default kernel-firmware-nvidia-gspx-G06 nvidia-video-G06 nvidia-gl-G06 nvidia-compute-G06 nvidia-compute-utils-G06
+
+#set firewallzone to block
 sudo firewall-cmd --set-default-zone block
+
 #virensignaturen aktualisieren
 sudo freshclam
 
-#opensuse doesn'T have obs-studio -- flathub
+#opensuse doesn't have obs-studio -- flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.obsproject.Studio
 
-#install freeplan mindmapping tool from flathub
-flatpak install flathub org.freeplane.App
+#install freeplan mindmapping tool from flathub --> I don't use this program currently
+#flatpak install flathub org.freeplane.App
+
+#add vlc-repo and install vlc-player
+sudo zypper addrepo https://download.videolan.org/SuSE/Tumbleweed VLC
+sudo zypper install vlc
 
 #virtual machines
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 
-sudo systemctl enable dkms.service
-sudo systemctl start dkms.service
-
-#tuxedo-keyboard an tuxedo-control-center via tuxedo-repos
-sudo zypper install tuxedo-control-center tuxedo-drivers
+# activate dkms. currently not necessary since I don't use packages activated via dkms anymore
+# sudo systemctl enable dkms.service
+# sudo systemctl start dkms.service
 
 #copy thunderbird-profiles
-cp -r ~/install/.thunderbird ~/
-
-#nvidia now via repos (open-kernel-modules)
-sudo zypper install nvidia-open-driver-G06-signed-kmp-default kernel-firmware-nvidia-gspx-G06 nvidia-video-G06 nvidia-gl-G06 nvidia-compute-G06 nvidia-compute-utils-G06
-
-#try without prime-select for now
-#sudo zypper install suse-prime bbswitch-kmp-default
-#sudo prime-select nvidia
+# cp -r ~/install/.thunderbird ~/
 
 #nvidia
-#zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
-#or directly from nvidia. Better since dkms works with self build zen-kernel
-#sudo zypper install kernel-devel kernel-source make dkms acpid libglvnd libglvnd-devel and supports nvidia-powerd
 echo 'blacklist nouveau' | sudo tee -a /etc/modprobe.d/nvidia.conf
-#echo 'add_drivers+=" nvidia nvidia_modeset nvidia_uvm nvidia_drm "' | sudo tee -a /etc/dracut.conf.d/nvidia.conf
-#Download nvidia driver from https://www.nvidia.de/Download/index.aspx?lang=de
-#safe "run" file in install directory
-#chmod +x ~/install/NVIDIA-Linux-x86_64-515.76.run
-#boot into cmd without nouveau by adding nomodeset 3 to grub entry during boot
-#login with root
-#then run NVIDIA Installer. dont blacklist (was already done above), no xconf, yes to dkms
-#sudo sh NVIDIA-Linux-x86_64-515.76.run
+
+#finish
 sudo dracut -f
 reboot
 
@@ -96,5 +88,5 @@ sudo systemctl start nvidia-powerd.service
 
 ############################################
 #manual changes for KDE
-- set adaptive-sync for external monitor to always under KDE to get maximum FPS on external monitor
-- set inactivity and when laptop lid closed to "Do nothing" under power options. The system might not recover from sleep. TODO: check, where the problem is. Maybe swap-space?
+# - set adaptive-sync for external monitor to always under KDE to get maximum FPS on external monitor
+# - set inactivity and when laptop lid closed to "Do nothing" under power options. The system might not recover from sleep. TODO: check, where the problem is. Maybe swap-space?
