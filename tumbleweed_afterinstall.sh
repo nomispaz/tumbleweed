c8d79aa3794758e247e76efe169f687ab8681665
nomispaz <nomispaz@example.org>
2025-10-04 10:46:08 +0200

    updated nvidia installation

--- tumbleweed_afterinstall.sh
+++ tumbleweed_afterinstall.sh
@@ -3,6 +3,9 @@
 #set hostname
 sudo hostnamectl set-hostname XMGneo15
 
+# TODO
+# remove nomodeset from kernel command line and recreate grub
+
 #remove bloat
 #sudo zypper remove -u discover6
 
@@ -26,7 +29,7 @@ sudo zypper install --allow-vendor-change ffmpeg
 sudo zypper dup --from nomispaz_local --allow-vendor-change
 
 # basic tools and apps
-sudo zypper install alacritty fish cpupower htop fastfetch keepassxc meld blueman gparted seahorse file-roller
+sudo zypper install alacritty fish cpupower htop fastfetch keepassxc meld blueman gparted seahorse file-roller libqt5-wayland
 
 # security
 sudo zypper install clamav
@@ -54,21 +57,43 @@ sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser
 sudo zypper install brave-browser
 
 # programming
-sudo dnf install rustup golang gopls elixir elixir-doc erlang
+sudo zypper install rustup golang gopls elixir elixir-doc erlang
 rustup toolchain install stable
 
 # osc as a cli for opensuse buildservice
 sudo zypper install osc
 
 # cosmic desktop
-sudo zypper addrepo --refresh https://download.opensuse.org/repositories/X11:COSMIC:Next/openSUSE_Factory/X11:COSMIC:Next.repo
-sudo zypper install patterns-cosmic-cosmic
+#sudo zypper addrepo --refresh https://download.opensuse.org/repositories/X11:COSMIC:Next/openSUSE_Factory/X11:COSMIC:Next.repo
+#sudo zypper install patterns-cosmic-cosmic
 
 # niri
 sudo zypper install niri
 
 # gaming
-sudo zypper install steam mangohud
+sudo zypper install steam mangohud wine
+
+# install from personal repo
+# add personal repo from buildservice
+sudo zypper addrepo https://download.opensuse.org/repositories/home:nomispaz/openSUSE_Tumbleweed/home:nomispaz.repo
+
+sudo zypper install veracrypt jdtls emacs-cape-git emacs-catppuccin-theme-git emacs-company-mode-git emacs-consult-git emacs-evil-git emacs-go-mode-git emacs-markdown-mode emacs-rust-mode-git emacs-spacemacs-theme-git emacs-yasnippet-git emacs-yasnippet-snippets-git elixir-ls codium nwg-panel
+
+#add nvidia repo
+sudo zypper install openSUSE-repos-Tumbleweed-NVIDIA
+
+# nvidia now via repos (open-kernel-modules)
+sudo zypper install --from NVIDIA:repo-non-free nvidia-open-driver-G06-signed-kmp-meta
+
+# install various programs
+# sudo zypper install clipgrab patterns-server-kvm_tools patterns-server-kvm_server spec-cleaner testdisk gcc gcc-c++
+sudo zypper install clipgrab spec-cleaner testdisk gcc gcc-c++
+
+#set firewallzone to block
+sudo firewall-cmd --set-default-zone block
+
+#virensignaturen aktualisieren
+sudo freshclam
 
 # bottles and heroic, flatseal, obs-studio
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -77,42 +102,10 @@ flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub com.heroicgameslauncher.hgl
 flatpak install flathub com.obsproject.Studio
 
-# install from personal repo
-# add personal repo from buildservice
-sudo zypper addrepo https://download.opensuse.org/repositories/home:nomispaz/openSUSE_Tumbleweed/home:nomispaz.repo
-
-sudo zypper install veracrypt jdtls emacs-cape-git emacs-catppuccin-theme-git emacs-company-mode-git emacs-compat-git emacs-consult-git emacs-evil-git emacs-go-mode-git emacs-markdown-mode emacs-rust-mode-git emacs-spacemacs-theme-git emacs-yasnippet-git emacs-yasnippet-snippets-git elixir-ls codium nwg-panel
-
-
-
-
-
-
-
-
-
-#add personal repo
-sudo zypper addrepo https://download.opensuse.org/repositories/home:nomispaz/openSUSE_Tumbleweed/home:nomispaz.repo
-
-#add nvidia repo
-sudo zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
-
-# install various programs
-sudo zypper install clipgrab patterns-server-kvm_tools patterns-server-kvm_server spec-cleaner testdisk gcc gcc-c++
-
-# nvidia now via repos (open-kernel-modules)
-sudo zypper install nvidia-open-driver-G06-signed-kmp-default kernel-firmware-nvidia-gspx-G06 nvidia-video-G06 nvidia-gl-G06 nvidia-compute-G06 nvidia-compute-utils-G06
-
-#set firewallzone to block
-sudo firewall-cmd --set-default-zone block
-
-#virensignaturen aktualisieren
-sudo freshclam
-
-sudo rkhunter --update
-sudo rkhunter --propupd
+# sudo rkhunter --update
+# sudo rkhunter --propupd
 #c for check q for skip keypress
-sudo rkhunter -c -sk
+# sudo rkhunter -c -sk
 
 #install freeplan mindmapping tool from flathub --> I don't use this program currently
 #flatpak install flathub org.freeplane.App
@@ -120,7 +113,7 @@ sudo rkhunter -c -sk
 #copy thunderbird-profiles
 # cp -r ~/install/.thunderbird ~/
 
-#nvidia
+#nvidia old way around 2023
 #echo 'blacklist nouveau' | sudo tee -a /etc/modprobe.d/nvidia.conf
 
 #finish
